dcf3c723b56e58ba9d0859d6fab1f984b03356f9
ypu1977 <ypu1977@naver.com>
2022-07-13 15:55:34 +0900

    1

--- src/com/iu/object1/ex1/StudentService.java
+++ src/com/iu/object1/ex1/StudentService.java
@@ -4,6 +4,11 @@ import java.util.Scanner;
 
 public class StudentService 
 {
+	//findStudent
+	//검색하고 싶은 학생의 번호를 입력받아서,
+	//같은 번호의 학생을 찾아서 그 학생한명을 리턴
+	//없으면 null을 리턴
+	
 	//makeStudents
 	//학생수를 입력 받고,
 	//학생수만큼 정보를 입력 받고,
--- src/com/iu/object1/ex1/StudentView.java
+++ src/com/iu/object1/ex1/StudentView.java
@@ -4,15 +4,18 @@ public class StudentView
 {	
 	//viewMessage
 	//문자열을 받아서 그 문자열을 출력
-	public void viewMessage()
+	public void viewMessage(String message)
 	{
-		
+		System.out.println(message);
 	}
 	//viewOne
 	//학생 한명의 정보를 받아서 모든 정보를 출력
-	public void viewOne()
+	public void viewOne(Student student)
 	{
-		
+		System.out.println(student.name );
+		System.out.println(student.num );
+		System.out.println(student.total );
+		System.out.println(student.avg );
 	}
 	
 	//viewAll
